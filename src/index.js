2784c0546d65d4da3a043508067ac0011977ef92
Jesse Ben-Emu <orexroxs@gmail.com>
2023-01-24 15:07:41 +0100

    new branch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello, React...again!</h1>
+      </div>
+    )
+  }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +18,7 @@ root.render(
   </React.StrictMode>
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
