ec5987929ed453ddb61eb3e40c4e1645fc8e3814
Jesse Ben-Emu <orexroxs@gmail.com>
2023-01-25 08:03:35 +0100

    second-branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {i
+    render() {
+      return (
+        <div className="App">
+          <h1>Hello, React...again!</h1>
+        </div>
+      )
+    }
+  }
+
+  export default App
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 import './index.css';
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <h1>Hello, React...again!</h1>
-      </div>
-    )
-  }
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<App />, document.getElementById('root'));
